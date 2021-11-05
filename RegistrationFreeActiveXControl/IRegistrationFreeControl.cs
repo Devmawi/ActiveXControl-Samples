249af233d9b4a86c29959325a2c798d2414e71a6
dev <dev@dev.org>
2021-11-05 13:01:00 +0100

    add registration free component

---
+++ RegistrationFreeActiveXControl/IRegistrationFreeControl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationFreeActiveXControl
+{
+    [ComVisible(true)]
+    [Guid("c24bb3a1-e36c-434c-b02b-22e4f3f2d2b5")]
+    public interface IRegistrationFreeControl
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the user control is visible.
+        /// </summary>
+        [DispId(1)]
+        bool Visible { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the user control is enabled.
+        /// </summary>
+        [DispId(2)]
+        bool Enabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets the foreground color of the user control.
+        /// </summary>
+        [DispId(3)]
+        int ForegroundColor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the background color of the user control.
+        /// </summary>
+        [DispId(4)]
+        int BackgroundColor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the background image of the user control.
+        /// </summary>
+        [DispId(5)]
+        Image BackgroundImage { get; set; }
+
+        /// <summary>
+        /// Forces the control to invalidate its client area and immediately redraw 
+        /// itself and any child controls.
+        /// </summary>
+        [DispId(6)]
+        void Refresh();
+
+        // add additional properties and methods visible in VB6
+
+        [DispId(7)]
+        int ControlWidth { get; set; }
+
+        [DispId(8)]
+        int ControlHeight { get; set; }
+        [DispId(9)]
+        string Url { get; set; }
+    }
+}
---
+++ RegistrationFreeActiveXControl/MyRegistrationFreeControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace RegistrationFreeActiveXControl
+{
+    [ComVisible(true)]
+    [Guid("667e9f57-67a4-4bd5-a64e-e4e067abf55d"), ClassInterface(ClassInterfaceType.None)]
+    [ProgId("RegistrationFreeActiveXControl.MyRegistrationFreeControl")]
+    [ComDefaultInterface(typeof(IRegistrationFreeControl))]
+    public partial class MyRegistrationFreeControl: UserControl, IRegistrationFreeControl
+    {
+        public MyRegistrationFreeControl()
+        {
+            InitializeComponent();
+        }
+
+        public int ForegroundColor { get; set; }
+        public int BackgroundColor { get; set; }
+        public int ControlWidth { get; set; }
+        public int ControlHeight { get; set; }
+        public string Url { get; set; }
+    }
+}
